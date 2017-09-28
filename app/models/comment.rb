2d215c530e27364217d72803051cdc56bb402368
Raghu Betina <raghu@firstdraft.com>
2017-09-28 23:04:45 +0000

    Direct association between images and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :photo,
+             :class_name => "Image",
+             :counter_cache => true
+
   belongs_to :commenting_user,
              :class_name => "User"
 
